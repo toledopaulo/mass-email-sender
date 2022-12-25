6c5bb992d2648ff2fe8feae48a8083a4f95607d0
toledopaulo <paulo.aff1803@gmail.com>
2022-12-25 00:38:31 -0300

    Project initial structure created.

---
+++ src/desktop/sender.py
@@ -0,0 +1,14 @@
+import requests
+
+class MailSender(object):
+    def __init__(self):
+        return
+
+    def auth(email, password):
+        pass
+
+    def send_simple_email():
+        pass
+
+    def send_email_with_attachments():
+        pass
